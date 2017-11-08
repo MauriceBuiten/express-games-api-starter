9da3f56f99e55e5da6bc432dcd3ca01f81fd83e6
mauricebuiten <maubui@gmail.com>
2017-11-08 10:57:10 +0100

    Add points and name to player schema

--- models/game.js
+++ models/game.js
@@ -2,16 +2,39 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
+
+
+// const gameSchema = new Schema({
+//   players: [playerSchema],
+//   letterBoard: { type: String, required: true },
+//   word: { type: String, required: true },
+//   guesses: { type: Array },
+//   completed: { type: Boolean },
+//   currentTurn: { type: Number, default: 0 },
+//   wheelValue: { type: Number, required: true },
+//   started: { type: Boolean, default: false },
+//   userId: { type: Schema.Types.ObjectId, ref: 'users' },
+//   createdAt: { type: Date, default: Date.now },
+//   updatedAt: { type: Date, default: Date.now },
+//   draw: { type: Boolean, default: false },
+// }, { usePushEach: true });
+
 const cardSchema = new Schema({
   symbol: { type: String, required: true },
   visible: { type: Boolean, default: false },
   won: { type: Boolean, default: false },
-});
+}, { usePushEach: true });
 
 const playerSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'users' },
-  pairs: [String],
-});
+  // name: { type: String, required: true},
+  points: { type: Number, default: 0 }
+}, { usePushEach: true });
+//
+// const playerSchema = new Schema({
+//   userId: { type: Schema.Types.ObjectId, ref: 'users' },
+//   pairs: [String],
+// }, { usePushEach: true });
 
 const gameSchema = new Schema({
   cards: [cardSchema],
@@ -24,6 +47,6 @@ const gameSchema = new Schema({
   updatedAt: { type: Date, default: Date.now },
   lastCard: { type: Number },
   draw: { type: Boolean, default: false },
-});
+}, { usePushEach: true });
 
 module.exports = mongoose.model('games', gameSchema)
